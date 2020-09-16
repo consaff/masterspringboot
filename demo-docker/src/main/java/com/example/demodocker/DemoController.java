c2904fce26535f1ff005612fb4c9c025af892555
consaff <70338644+consaff@users.noreply.github.com>
2020-09-16 16:05:21 -0700

    Update DemoController.java

--- demo-docker/src/main/java/com/example/demodocker/DemoController.java
+++ demo-docker/src/main/java/com/example/demodocker/DemoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
+import io.micrometer.prometheus.PrometheusConfig;
 
 
 @RestController
