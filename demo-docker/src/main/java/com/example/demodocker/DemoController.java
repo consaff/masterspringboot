0dbf1c52fb17fb298eb6432cbfc775102f353ec0
consaff <70338644+consaff@users.noreply.github.com>
2020-10-22 12:39:33 -0700

    Update DemoController.java

--- demo-docker/src/main/java/com/example/demodocker/DemoController.java
+++ demo-docker/src/main/java/com/example/demodocker/DemoController.java
@@ -15,9 +15,11 @@ public class DemoController
     @RequestMapping("/metrics")
     public String test()
     {
-        String ret = "# HELP test metric" 
-            + "\n# TYPE test gauge"
-            + "\ntest 10.0";
+        String ret = 
+        "# HELP jvm_memory_used_bytes The amount of used memory \n# TYPE jvm_memory_used_bytes gauge \njvm_memory_used_bytes{area="heap",id="PS Survivor Space",} 0.0" +
+            "\njvm_memory_used_bytes{area="heap",id="PS Old Gen",} 1.1718296E7 \njvm_memory_used_bytes{area="heap",id="PS Eden Space",} 1.07929016E8" +
+            "\njvm_memory_used_bytes{area="nonheap",id="Metaspace",} 3.9945752E7 \njvm_memory_used_bytes{area="nonheap",id="Code Cache",} 1.074304E7"
+            + "\njvm_memory_used_bytes{area="nonheap",id="Compressed Class Space",} 4938248.0";
         
         return ret;       
     }
