b07b0361bd782c822973c973cc12f4b54cf96eb4
consaff <70338644+consaff@users.noreply.github.com>
2020-10-27 09:18:56 -0700

    Update DemoController.java

--- demo-docker/src/main/java/com/example/demodocker/DemoController.java
+++ demo-docker/src/main/java/com/example/demodocker/DemoController.java
@@ -15,13 +15,19 @@ public class DemoController
     @RequestMapping("/fakemetrics")
     public String test()
     {
-        String ret = 
+        String jvm_buffer_memory_used_bytes = 
             "# HELP jvm_buffer_memory_used_bytes An estimate of the memory that the Java virtual machine is using for this buffer pool\n"
             + "# TYPE jvm_buffer_memory_used_bytes gauge\n"
-            + "jvm_buffer_memory_used_bytes{id=\"direct\"} 81920.0\n"
-            + "# EOF\n";
+            + "jvm_buffer_memory_used_bytes{id=\"direct\"} 81920.0\n";
         
-        return ret;       
+        String jvm_memory_max_bytes =
+            "# HELP jvm_memory_max_bytes The maximum amount of memory in bytes that can be used for memory management\n"
+            + "# TYPE jvm_memory_max_bytes gauge\n"
+            + "jvm_memory_max_bytes{area=\"heap\", id=\"PS Survivor Space\"} 2097152.0\n"
+            + "jvm_memory_max_bytes{area=\"heap\", id=\"PS Old Gen\"} 5.54067E9\n"
+            + "jvm_memory_max_bytes{area=\"heap\", id=\"PS EdenSpace\"} 2.2734E9\n";
+        
+        return jvm_buffer_memory_used_bytes + jvm_memory_max_bytes + "# EOF\n";       
     }
     
     //PrometheusMeterRegistry prometheusRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
